3548db6ebc1599bba7c6f88a23671a8dc45cec95
Marlie Ford <jive.hive@outlook.com>
2023-09-11 23:59:40 -0500

    done !

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Assignment code here
-
-var lowercaseChar = false;
-var upperCaseChar = false;
-var numericChar = false;
-var specialChar = false;
-
+// generate password
 var upperCaseValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCaseValues = "abcdefghijklmnopqrstuvwxyz";
 var numericValues = "0123456789";
@@ -12,28 +6,7 @@ var specialValues = "!@#$%^&*()_+-=?><,./{}[]:|~//\\";
 
 
 var generateBtn = document.querySelector("#generate");
-
-var specialCharacters = [
-  
-]
-
-function setLowercaseChar(){
-  lowercaseChar = !lowercaseChar;
-  console.log(lowercaseChar);
-};
-
-function setUppercaseChar(){
-  uppercaseChar = !uppercaseChar;
-};
-
-function setNumericChar(){
-  numericChar = !numericChar;
-};
-
-function setSpecialChar(){
-  specialChar = !specialChar;
-};
-
+// choose pass criteria
 var upperCase;
 var lowerCase;
 var numericCharacters;
@@ -41,58 +14,44 @@ var specialCharacters;
 var howLong;
 
 function generatePassword(){
-    upperCase = prompt("Do you want uppercase letters in your password? Type 'Yes' or 'No' " ).toLowerCase() == "yes";
-    lowerCase = prompt("Do you want lowercase letters in your password? Type 'Yes' or 'No' ").toLowerCase() == "yes";
-    numericCharacters = prompt("Do you want numeric characters in your password? Type 'Yes' or 'No' ").toLowerCase() == "yes";
-    specialCharacters = prompt("Do you want special characters in your password? Type 'Yes' or 'No' ").toLowerCase() == "yes";
+    upperCase = prompt("Would you like uppercase letters in your password? Type 'Yes' or 'No' " ).toLowerCase() == "yes";
+    lowerCase = prompt("Would you like lowercase letters in your password? Type 'Yes' or 'No' ").toLowerCase() == "yes";
+    numericCharacters = prompt("Would you like numeric characters in your password? Type 'Yes' or 'No' ").toLowerCase() == "yes";
+    specialCharacters = prompt("Would you like special characters in your password? Type 'Yes' or 'No' ").toLowerCase() == "yes";
     howLong = parseInt(prompt("How many characters long would you like your password to be? Type a number value between 8 and 128"));
-var values = "";
+    
+    var values = "";
 
-if(upperCase){
-  values += upperCaseValues
-}
+    if(upperCase){
+      values += upperCaseValues
+    }
 
-if(lowerCase){
-  values += lowerCaseValues
-}
+    if(lowerCase){
+      values += lowerCaseValues
+    }
 
-if(numericCharacters){
-  values += numericValues
-}
+    if(numericCharacters){
+      values += numericValues
+    }
 
-if(specialCharacters){
-  values += specialValues
-}
+    if(specialCharacters){
+      values += specialValues
+    }
 
-let password = "";
+    let password = "";
 
-for (var i = 0 ; i < howLong; i++){
-  password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-}
+    for (var i = 0 ; i < howLong; i++){
+      password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
 document.getElementById("password").value = password;
+
 return password;
 };
 
-
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-// Get references to the #generate element
+// get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -101,11 +60,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
